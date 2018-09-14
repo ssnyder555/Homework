8e23fc49c6794b85d55e031c8333c3f3dd541145
Stephen Snyder <ssnyder555@live.com>
2018-09-14 09:31:46 -0500

    9-13-homework

--- 9-12-homework/javascript/9-12-homework.js
+++ 9-12-homework/javascript/9-12-homework.js
@@ -1,1 +1,67 @@
 console.log('js works');
+
+
+// Write a for loop that will log the numbers 1 through 20.
+
+
+
+
+// Write a for loop that will log only the even numbers in 0 through 200.
+
+
+
+// Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+
+
+//For every even number in your loop, log
+ // "...human...why you taking pictures of me?...",
+  // "...the catnip made me do it...", or "...why does the red dot always get away..."
+   // at random.
+
+
+   // Write a javascript application that logs all numbers from 1 - 100.
+// If a number is divisible by 3 log "Fizz" instead of the number.
+// If a number is divisible by 5 log "Buzz" instead of the number.
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+for ( var i = 0 ; i < 20 ; i++) {
+            if ( i % 3) {
+                console.log("Fizz");
+            }
+            else if( i % 5) {
+                console.log("Buzz");
+            }
+            else if(i % 3 || 5) {
+                console.log("FizzBuzz");
+            }
+            else {
+                console.log(i);
+            }
+        }
+
+// Use the following arrays to answer the questions below (name, age, hometown):
+
+const shahzad = ["Shahzad", 1000, "Austin"];
+const jim = ["Jim", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const daniel = ["Daniel", 186, "Dallas"];
+const ryan = ["Ryan", 65, "Denver"];
+
+
+// Daniel decides that Shahzad can't be named "Shahzad" anymore.
+ // Remove "Shahzad" from the shahzad array and replace it with "Gameboy".
+
+// Daniel just had his birthday; change Daniel's array to reflect him
+// being a year older. Don't just hard code 187--pretend that you don't
+// know that his age is 186, and write your code to just make him a year
+// older than whatever age he was.
+
+// Ryan is Batman maybe. Or possibly Robin. Change Ryan's
+// hometown from "Denver" to "Gotham City".
+
+// Reuben left Durham 5 years ago to come to Chicago. Remove "Durham"
+// from Reuben's array and add "Chicago". (Note: remove and then add is different
+  // from simply changing the value at that index.)
+
+// Jim could be literally anywhere in the world. Remove "All cities" from his
+// array, then pick any 3 cities you like, and add them to Jim's array. If you
+// did it in 3 lines of code that's fine, but see if you can do it in one line of code
---
+++ javascript/app.js
@@ -0,0 +1,63 @@
+console.log('working');
+
+
+//What is the difference between a parameter and an argument?
+
+// A parameter is a variable in a method definition.
+// When a method is called, the arguments are the data you
+ // pass into the method's parameters. Parameter is variable
+ // in the declaration of function. Argument is the actual value of
+ // this variable that gets passed to function.
+
+
+// Within a function, what is the difference between
+// return and console.log?
+
+// 1 will be stored in the variable a. Return is
+// the result a subroutine, or function, will return,
+//and console.log is a way to log to the "console" where
+// available. The console is a destination made available
+// by certain browsers that enables Javascript programmers
+// to stream text to an output e.g logging.
+
+// What are the implications of the ability of
+// a function to return a value?
+
+
+// 12.4 Functions that Return Values.
+// A return is a value that a function returns to the
+// calling script or function when it completes its
+// task. A return value can be any one of the four variable
+// types: handle, integer, object, or string. The type of value
+// your function returns depends largely on the task it performs.
+
+ // 2. Write a function checkPalindrome that accepts a
+// single argument, a string. Yes, you've done it before,
+// but do it again. Later in this assignment we're gonna beef up
+// our palindrome function some. See if you can do it without looking
+// back at your previous answer. The function should return true if
+// the string is a palindrome, false if not. Make sure your function
+// will give the correct answer for words with capital letters.
+
+
+
+
+// const checkPalidrome = (word) => {
+   // console.log(word.split(" "));
+// }
+
+
+// console.log(checkPalindrome("Radar"));
+ // => true
+// console.log(checkPalindrome("Borscht"));
+// => false
+
+// 3. Write a function sumDigits that accepts a number
+// and returns the sum of its digits.
+
+const sumDigits = (i ==> 0){
+  
+}
+
+console.log(sumDigits(42));
+=> 6;
