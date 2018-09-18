40eeb5d4e42eecad6117fbfb7c907dbd8dd43abb
Stephen Snyder <ssnyder555@live.com>
2018-09-18 09:29:44 -0500

    data types

---
+++ week-2-homework/9-17-homework/js/app.js
@@ -0,0 +1,25 @@
+console.log('working');
+
+
+// A light switch that can be either on or off
+
+const lightSwitch = {}
+// A user's email address.
+@page.com
+// A spaceship with a hull, laser blasters, tractor beam,
+// and warp drive.
+const spaceShip = {
+  body: 'hull',
+  defense: 'laser beams',
+  shield: 'trator beam',
+  engine: 'warp drive'
+}
+// A list of student names from our class.
+
+const WebDev class = ['Stephen', + 'Mark', + 'Amy']
+// A list of student names from our class, each with a location.
+
+function location = ['home', 'class', 'work'];
+// A list of student names from our class, each with a location and
+// each with a list of favorite tv shows
+function tvShows = [ if and what is who to why]
