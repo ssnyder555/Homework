80e3469f6190104399ef36fcc7bc79a9fc1c7e9a
Stephen Snyder <ssnyder555@live.com>
2018-10-14 14:55:11 -0500

    first commit

---
+++ weekend-hw/men-stack/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+// const bodyParser = require('body-parser')
+// const methodOverride = require('method-override')
+//
+// require('./db/db');
+//
+// const destinationsController = require('./controllers/dC')
+//
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(methodOverride('_method'));
+// app.use('/destinations', destinationsController);
+//
+// // app.get('/', (req,res) => {
+// //   res.send('It is Aliveeeeeee');
+// // })
+
+
+app.listen(3000, () => {
+  console.log('App is alive');
+})
