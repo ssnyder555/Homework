9338daf51d6a9b56bc2af0ca3cca0b68bda02a9f
Stephen Snyder <ssnyder555@live.com>
2018-10-14 16:54:24 -0500

    list printed

--- weekend-hw/men-stack/models/music.js
+++ weekend-hw/men-stack/models/music.js
@@ -1,30 +1,30 @@
-const mongoose = require('mongoose');
-
-const musicSchema = new mongoose.Schema({
-  artiest: {type: String, required: true},
-  genre: {type: String},
-  goodWithBass: {type: Boolean},
-  image: {type: String}
-});
+// const mongoose = require('mongoose');
+//
+// const musicSchema = new mongoose.Schema({
+//   artiest: {type: String, required: true},
+//   genre: {type: String},
+//   goodWithBass: {type: Boolean},
+//   image: {type: String}
+// });
 
 const music = [{
   artiest: 'Mac Miller',
   genre: 'Hip Hop',
   goodWithBass: true,
   image: 'https://pixel.nymag.com/imgs/daily/vulture/2018/09/05/mac-miller/04-mac-miller-6.w1200.h630.jpg'
-};
+},
   {
     artiest: 'Beetoven',
     genre: 'Classical',
     goodWithBass: 'false',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Beethoven.jpg/220px-Beethoven.jpg'
-  };
+  },
   {
     artiest: 'Bassnectar',
     genre: 'Dub Step',
     goodWithBass: true,
     image: 'https://edm.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTUzMjA4MDExMjYxNzQ4MjMx/bassnectar.jpg'
-  };
-]
+  }
+];
 
-module.exports = mongoose.model('Music', musicSchema);
+module.exports = music
--- weekend-hw/men-stack/server.js
+++ weekend-hw/men-stack/server.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const app = express();
-const Music = require('../models/music');
- const bodyParser = require('body-parser')
- const methodOverride = require('method-override')
+const Music = require('./models/music');
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
- app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
- app.get('/', (req,res) => {
-   console.log('clicked');
-   res.send('It is Aliveeeeeee');
+app.get('/', (req, res) => {
+  res.send('It is Aliveeeeeee')
 })
 
 app.get('/music/:index', (req, res) => {
   res.render('index.ejs', {
-    music: Music
+    list: Music[req.params.index]
   });
+});
+app.get('/music', (req, res) => {
+  res.send(Music)
 })
+
 
 app.listen(3000, () => {
   console.log('App is alive');
-})
+});
