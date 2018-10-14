450333a322b561d7f1c4ddb187f5ef6d6343241c
Stephen Snyder <ssnyder555@live.com>
2018-10-14 16:23:55 -0500

    images in

--- weekend-hw/men-stack/models/music.js
+++ weekend-hw/men-stack/models/music.js
@@ -7,4 +7,24 @@ const musicSchema = new mongoose.Schema({
   image: {type: String}
 });
 
+const music = [{
+  artiest: 'Mac Miller',
+  genre: 'Hip Hop',
+  goodWithBass: true,
+  image: 'https://pixel.nymag.com/imgs/daily/vulture/2018/09/05/mac-miller/04-mac-miller-6.w1200.h630.jpg'
+};
+  {
+    artiest: 'Beetoven',
+    genre: 'Classical',
+    goodWithBass: 'false',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Beethoven.jpg/220px-Beethoven.jpg'
+  };
+  {
+    artiest: 'Bassnectar',
+    genre: 'Dub Step',
+    goodWithBass: true,
+    image: 'https://edm.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTUzMjA4MDExMjYxNzQ4MjMx/bassnectar.jpg'
+  };
+]
+
 module.exports = mongoose.model('Music', musicSchema);
--- weekend-hw/men-stack/server.js
+++ weekend-hw/men-stack/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
-
+const Music = require('../models/music');
  const bodyParser = require('body-parser')
  const methodOverride = require('method-override')
 
-// require('./db/db');
-//
-// const destinationsController = require('./controllers/dC')
-//
  app.use(bodyParser.urlencoded({extended: false}));
-// app.use(methodOverride('_method'));
-// app.use('/destinations', destinationsController);
-//
+
  app.get('/', (req,res) => {
+   console.log('clicked');
    res.send('It is Aliveeeeeee');
 })
 
+app.get('/music/:index', (req, res) => {
+  res.render('index.ejs', {
+    music: Music
+  });
+})
 
 app.listen(3000, () => {
   console.log('App is alive');
