b9416654b644fe0bcdd80dd8ff7d5fe23c70596d
Stephen Snyder <ssnyder555@live.com>
2018-10-14 18:16:38 -0500

    new.ejs and buttons

--- weekend-hw/men-stack/server.js
+++ weekend-hw/men-stack/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 // require database
-require('./db/db'); 
+require('./db/db');
 
 // middleware
 app.use(bodyParser.urlencoded({extended: false}));
@@ -23,6 +23,11 @@ app.get('/music', (req, res) => {
       });
     }
   })
+});
+
+// new route
+app.get('/music/new', (req, res) => {
+  res.render('new.ejs')
 });
 
 // post route
@@ -51,6 +56,38 @@ app.get('/food/:index', (req, res) => {
     })
   })
 });
+
+// delete route
+app.delete('/music/:index', (req, res) => {
+  Music.findByIdAndRemove(req.params.index, (err, foundMusic) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/music');
+    }
+  });
+});
+
+// edit that route
+app.get('/music/:index/edit', (req, res) => {
+  Music.findById(req.params.index, (err, foundMusic) => {
+    res.render('edit.ejs', {
+      music: foundMusic
+    });
+  });
+});
+
+// update thy route
+app.put('/music/:index', (req, res) => {
+  if (req.body.goodWithBass === 'on') {
+    req.body.goodWithBass = true;
+  } else {
+    req.body.goodWithBass = false;
+  }
+  Music.findByIdAndUpdate(req.params.index, req.body, (err, updatedModel) => {
+    res.redirect('/music');
+  })
+})
 
 
 
