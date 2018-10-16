1b19b6d9126b37f236f5812d2e17573fb165f531
Stephen Snyder <ssnyder555@live.com>
2018-10-16 10:08:13 -0500

    server.js kinda wonky

--- 6-wk-hw/photo/server.js
+++ 6-wk-hw/photo/server.js
@@ -1,10 +1,25 @@
 const express    = require('express');
 const app        = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+// const authorsController = require('./controllers/authors');
+
+require('./db/db');
+
+const usersController = require('./controllers/users');
 
 
+// make sure to require this before our controller
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+// setting up the middleware for our controller
+// where every route will start with /authors
+app.use('/users', usersController);
 
 
-
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
 
 
 app.listen(3000, () => {
