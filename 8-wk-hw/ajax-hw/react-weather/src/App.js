1125fafb78174f4b923941ba44d672122d95cfb9
Stephen Snyder <ssnyder555@live.com>
2018-10-31 10:16:09 -0500

    MainContainer started

--- 8-wk-hw/ajax-hw/react-weather/src/App.js
+++ 8-wk-hw/ajax-hw/react-weather/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Login from './Login';
 import './App.css';
+import MainContainer from './MainContainer';
 
 
 
---
+++ 8-wk-hw/ajax-hw/react-weather/src/MainContainer/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class MainContainer extends Component {
+
+
+
+render(){
+  return (
+    <div>
+   <h1>Weather Api</h1>
+   </div>
+  )
+}
+}
+
+export default MainContainer;
