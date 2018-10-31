7e9878d3953015444fbc3e27985c0e19c585e8c9
Stephen Snyder <ssnyder555@live.com>
2018-10-31 10:05:58 -0500

    Login working

--- 8-wk-hw/ajax-hw/react-weather/src/App.js
+++ 8-wk-hw/ajax-hw/react-weather/src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+// import logo from './logo.svg';
+import Login from './Login';
 import './App.css';
 
+
+
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      logged: false,
+      username: ''
+    }
+  }
+  handleLogin = (username, isLoggedIn) => {
+    console.log(username, isLoggedIn, ' in handleLogin');
+
+    this.setState({
+      username: username,
+      logged: isLoggedIn
+    });
+  }
   render() {
     return (
       <div className="App">
+      {this.state.logged ? <Login username={this.state.username}/> : <Login login={this.login}/>}
       </div>
     );
   }
